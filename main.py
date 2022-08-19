3744a3cc06d9cc9a9c4e09f2da1ceea66d8b54a8
yerin <tjtlsch62@naver.com>
2022-08-19 21:11:59 +0900

    study

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-#test
+import numpy as np
+import matplotlib.pylab as plt
 
-#test2
+def step_function(x):
+    return np.array(x > 0, dtype = np.int64)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+
+plt.plot(x,y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+
+def sigmoide(x):
+    return 1/ (1+ np.exp(-x))
+
+x=np.array([-1.0, 1.0, 2.0])
+print(sigmoide(x))
+
+
+
+#3층 신경망 구현하기
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.4], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
